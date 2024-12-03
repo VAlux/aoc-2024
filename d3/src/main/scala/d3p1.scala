8877e14760fb1ac00d32fb571a57483350efdf63
VAlux <olex3000sp@gmail.com>
2024-12-03 21:01:02 +0200

    d3p1 done

--- d3/src/main/scala/d3p1.scala
+++ d3/src/main/scala/d3p1.scala
@@ -1,2 +1,27 @@
 object d3p1 extends Solution[Int]:
-  override def solve(input: List[String]): Int = 0
+  val mulRegex = s"mul\\(\\d+,\\d+\\)".r
+
+  enum Operation:
+    case MUL
+    case NOP
+
+  case class Instruction(operation: Operation, operands: List[String])
+  object Instruction:
+    val NOP = Instruction(Operation.NOP, List.empty)
+
+  def parseInstruction(input: String): Instruction =
+    Instruction(
+      Operation.MUL,
+      input.substring(input.indexOf("(") + 1, input.indexOf(")")).split(",").toList
+    )
+
+  def interpretInstruction(instruction: Instruction): Int =
+    instruction.operation match
+      case Operation.MUL => instruction.operands.map(_.toInt).reduce(_ * _)
+      case Operation.NOP => 0
+
+  def extactAllInstructions(input: String): List[Instruction] =
+    mulRegex.findAllIn(input).map(parseInstruction).toList
+
+  override def solve(input: List[String]): Int =
+    input.flatMap(extactAllInstructions).map(interpretInstruction).sum
