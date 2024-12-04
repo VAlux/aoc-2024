3e927acce984911e871e2ff1f778fa62f2cf7669
VAlux <olex3000sp@gmail.com>
2024-12-04 17:45:23 +0200

    d4p2 done

--- d4/src/main/scala/d4p1.scala
+++ d4/src/main/scala/d4p1.scala
@@ -1,8 +1,6 @@
 import scala.annotation.static
 
 object d4p1 extends Solution[Int]:
-  val xmasRay = List('X', 'M', 'A', 'S')
-
   case class Coord(row: Int, column: Int):
     def offset(delta: Int): Coord       = Coord(row + delta, column + delta)
     def offset(dr: Int, dc: Int): Coord = Coord(row + dr, column + dc)
@@ -24,22 +22,23 @@ object d4p1 extends Solution[Int]:
 
     List(w, e, n, s, nw, ne, se, sw).filter(_.forall(_.inBounds(width, height)))
 
-  def locateAllCenters(input: List[String]): List[Coord] =
+  def locateAllCenters(input: List[String], targetCharacter: Char): List[Coord] =
     input.zipWithIndex.flatMap: (line, rowIndex) =>
       line
         .toCharArray()
         .zipWithIndex
-        .filter((char, _) => char == 'X')
+        .filter((char, _) => char == targetCharacter)
         .map((_, columnIndex) => Coord(rowIndex, columnIndex))
         .toList
 
-  def checkStarIndexes(starIndexes: List[List[Coord]], input: List[Array[Char]]): Int =
+  def checkIndexes(starIndexes: List[List[Coord]], input: List[Array[Char]], target: List[Char]): Int =
     starIndexes
       .map(starRay => starRay.map(coord => input(coord.row)(coord.column)))
-      .count(_ == xmasRay)
+      .count(_ == target)
 
   override def solve(input: List[String]): Int =
-    locateAllCenters(input)
+    val xmasRay = List('X', 'M', 'A', 'S')
+    locateAllCenters(input, 'X')
       .map(center => generateStarIndexes(center, input.head.size, input.size))
-      .map(checkStarIndexes(_, input.map(_.toCharArray())))
+      .map(checkIndexes(_, input.map(_.toCharArray()), xmasRay))
       .sum
--- d4/src/main/scala/d4p2.scala
+++ d4/src/main/scala/d4p2.scala
@@ -1,3 +1,14 @@
 import d4p1.*
+
 object d4p2 extends Solution[Int]:
-  override def solve(input: List[String]): Int = 0
+  def generateCrossIndexes(center: Coord, width: Int, height: Int): List[List[Coord]] =
+    val nw = List(center.offset(-1), center, center.offset(1))
+    val ne = List(center.offset(-1, 1), center, center.offset(1, -1))
+
+    List(nw, nw.reverse, ne, ne.reverse).filter(_.forall(_.inBounds(width, height)))
+
+  override def solve(input: List[String]): Int =
+    val masRay = List('M', 'A', 'S')
+    locateAllCenters(input, 'A')
+      .map(center => generateCrossIndexes(center, input.head.size, input.size))
+      .count(checkIndexes(_, input.map(_.toCharArray()), masRay) == 2)
